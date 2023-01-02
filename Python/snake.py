e160d25962755ab9dfa70318ac6b62176053f12d
alexgimenez02 <alex.gimenez.saludes@gmail.com>
2023-01-02 14:19:38 +0100

    refactor(python): fixed the issues with the python game

--- Python/snake.py
+++ Python/snake.py
@@ -1,205 +1,202 @@
 import curses
 import random
 import time
-speed = 1.0
-(board_height, board_width) = (10, 20)
-exit = 2
-# Initialize the game window
+import threading
 
-stdscr = curses.initscr()
-curses.noecho()
-curses.cbreak()
-stdscr.keypad(True)
+def clamp(num, lowerLimit = 0, upperLimit = 1):
+    if num < lowerLimit:
+        return lowerLimit
+    if num > upperLimit:
+        return upperLimit
+    return num
 
-# Set up the game board
+# Set the initial direction of the snake
+snake_direction = curses.KEY_RIGHT
 
-board = [['.' for _ in range(board_width)] for _ in range(board_height)]
+# Create a flag to indicate when the game should stop
+stop_flag = False
 
-# Set up the snake
+# Set the initial menu selection
+menu_selection = 2
 
-snake_positions = [(4, 5), (4, 6), (4, 7)]
-snake_direction = curses.KEY_LEFT  # Set the initial direction of the snake
+# Set the maximum number of menu items
+max_menu_items = 4
 
-# Set up the food
+def input_thread(stdscr):
+    global snake_direction, stop_flag
 
-food_position = None
-
-stdscr.nodelay(True)
-
-def refreshBoard():
-    board = [['.' for _ in range(board_width)] for _ in range(board_height)]
-    
-def config():
-    while True:
-        # Display the configuration menu
-
-        stdscr.clear()
-        stdscr.addstr(0, 0, 'Configuration')
-        stdscr.addstr(2, 0, '1. Change board size')
-        stdscr.addstr(3, 0, '2. Change difficulty level')
-        stdscr.addstr(4, 0, '3. Return to main menu')
-        stdscr.refresh()
-
-        # Get the user's configuration selection
-
+    while not stop_flag:
+        # Read the user's input
         key = stdscr.getch()
-        if key == ord('1'):
 
-          # Allow the user to change the size of the game board
-            stdscr.clear()
-            stdscr.addstr(0, 0, 'Board size')
-            stdscr.addstr(2, 0, '1. 10x20')
-            stdscr.addstr(3, 0, '2. 20x40')
-            stdscr.addstr(4, 0, '3. 30x60')
-            stdscr.refresh()
-            key = stdscr.getch()
-            if key == ord('1'):
-                (board_height, board_width) = (10, 20)
-            elif key == ord('2'):
-                (board_height, board_width) = (20, 40)
-            elif key == ord('3'):
-                (board_height, board_width) = (30, 60)
-            refreshBoard()
-        elif key == ord('2'):
+        # Update the snake's direction based on the user's input
+        if key == curses.KEY_UP:
+            snake_direction = key
+        elif key == curses.KEY_DOWN:
+            snake_direction = key
+        elif key == curses.KEY_LEFT:
+            snake_direction = key
+        elif key == curses.KEY_RIGHT:
+            snake_direction = key
+            
 
-          # Allow the user to change the difficulty level
-            stdscr.clear()
-            stdscr.addstr(0, 0, 'Difficulty')
-            stdscr.addstr(2, 0, '1. Easy')
-            stdscr.addstr(3, 0, '2. Medium')
-            stdscr.addstr(4, 0, '3. Hard')
-            stdscr.refresh()
-            key = stdscr.getch()
-            if key == ord('1'):
-                speed = 1.5
-            elif key == ord('2'):
-                speed = 1.0
-            elif key == ord('3'):
-                speed = 0.5
-        elif key == ord('3'):
-          # Return to the main menu
-          break
-    menu()
-def game():
-    food_position = None
+def main(stdscr):
+
+    # Create a new thread for reading the user's input
+    global menu_selection, snake_direction, stop_flag
+    board_height = 20
+    board_width = 20
+    snake_speed = 0.5
+    # Initialize the game board
+    food_spawned = False
+    snake_direction = curses.KEY_RIGHT
     while True:
+        display_menu(stdscr)
+         # Read the user's input
+        key = stdscr.getch()
+         # Update the menu selection based on the user's input
+        if key == curses.KEY_DOWN:
+            menu_selection = (menu_selection - 1) % max_menu_items + 2
+        elif key == curses.KEY_UP:
+            menu_selection = (menu_selection + 1) % max_menu_items + 2
+        elif key == curses.KEY_ENTER or key == 10:
+            # The user has selected an option, process it
+            if menu_selection == 2:
+                # Start the game
+                break
+            elif menu_selection == 3:
+                # Change the board size
+                change_board_size(stdscr)
+            elif menu_selection == 4:
+                # Change the difficulty
+                change_difficulty(stdscr)
+            elif menu_selection == 5:
+                # Quit the game
+                stdscr.clear()
+                stdscr.addstr('Bye!', curses.color_pair(2))
+                stdscr.getch()
+                return
+    board = [['.' for x in range(board_width)] for y in range(board_height)]
+    snake = [(board_height // 2, board_width // 2)]
+    inp_thread = threading.Thread(target=input_thread,args=(stdscr,))
 
-        #Clear the screen
+    # Start the input thread
+    inp_thread.start()
+    
+    
+    stdscr.nodelay(True)    
+    # Main game loop
+    while True:
+        # Check for input from the player
+        
+        # Update the snake's position
+        new_head = None
+        if snake_direction == curses.KEY_UP:
+            new_head = (snake[0][0] - 1, snake[0][1])
+        elif snake_direction == curses.KEY_DOWN:
+            new_head = (snake[0][0] + 1, snake[0][1])
+        elif snake_direction == curses.KEY_LEFT:
+            new_head = (snake[0][0], snake[0][1] - 1)
+        elif snake_direction == curses.KEY_RIGHT:
+            new_head = (snake[0][0], snake[0][1] + 1)
 
-        stdscr.clear()
-        #Draw the game board
+        snake.insert(0, new_head)
 
+        # Check for collision with the food
+        if not food_spawned or new_head != (foodY, foodX):
+            snake.pop()
+        else:
+            food_spawned = False
+
+        # Check for collision with the walls
+        if (new_head[0] < 0 or new_head[0] >= board_height or
+                new_head[1] < 0 or new_head[1] >= board_width or
+                new_head in snake[1:]):
+            stop_flag = True
+            break
+
+        # Spawn food if there is none on the board
+        if not food_spawned:
+            food_spawned = True
+            foodY, foodX = spawn_food(board, snake, board_width, board_height)
+
+        # Update the game board
         for y in range(board_height):
             for x in range(board_width):
-                stdscr.addch(y, x, board[y][x])
+                board[y][x] = '.'
+        for i in range(len(snake)):
+            board[snake[i][0]][snake[i][1]] = 'S'
+        board[foodY][foodX] = 'F'
 
-      # Draw the snake
+        # Draw the game board
+        stdscr.clear()
+        for y in range(board_height):
+            for x in range(board_width):
+                if board[y][x] == 'S':
+                    stdscr.addch(y, x, board[y][x], curses.color_pair(2))
+                elif board[y][x] == 'F':
+                    stdscr.addch(y, x, board[y][x], curses.color_pair(1))
+                else:
+                    stdscr.addch(y, x, board[y][x])
 
-        for (y, x) in snake_positions:
-            stdscr.addch(y, x, 'S')
+        # Wait for the next frame
+        time.sleep(snake_speed)
+    # Set the stop flag to indicate that the game should stop
+    stop_flag = True
 
-      # Draw the food
-
-        if food_position is not None:
-            (y, x) = food_position
-            stdscr.addch(y, x, 'F')
-        key = stdscr.getch()
-        if key == curses.KEY_UP:
-            snake_direction = curses.KEY_UP
-        elif key == curses.KEY_DOWN:
-            snake_direction = curses.KEY_DOWN
-        elif key == curses.KEY_LEFT:
-            snake_direction = curses.KEY_LEFT
-        elif key == curses.KEY_RIGHT:
-            snake_direction = curses.KEY_RIGHT
-
-      # Update the snake's position
-
-        (y, x) = snake_positions[0]
-        if snake_direction == curses.KEY_UP:
-            snake_positions.insert(0, (y - 1, x))
-        elif snake_direction == curses.KEY_DOWN:
-            snake_positions.insert(0, (y + 1, x))
-        elif snake_direction == curses.KEY_LEFT:
-            snake_positions.insert(0, (y, x - 1))
-        elif snake_direction == curses.KEY_RIGHT:
-            snake_positions.insert(0, (y, x + 1))
-
-      # Check for collision with the edges of the board
-
-        (y, x) = snake_positions[0]
-        if y < 0 or y >= board_height or x < 0 or x >= board_width:
-            break
-
-      # Check for collision with the snake's body
-
-        if snake_positions[0] in snake_positions[1:]:
-            break
-
-      # Check for collision with the food
-
-        if snake_positions[0] == food_position:
-            food_position = None
-        else:
-            snake_positions.pop()
-
-      # If there is no food, generate a new piece of food
-
-        if food_position is None:
-            food_position = (random.randint(0, board_height - 1),
-                             random.randint(0, board_width - 1))
-
-      # Update the display
-
-        stdscr.refresh()
-
-      # Wait for a short period of time before moving the snake again
-
-        time.sleep(speed)
-
-
-
-def menu():
-    stdscr.clear()
-    stdscr.addstr(0, 0, 'Welcome to Snake!')
-    stdscr.addstr(2, 0, '1. Start game')
-    stdscr.addstr(3,0, '2. Configuration')
-    stdscr.addstr(4, 0, '3. Quit')
-    stdscr.refresh()
+    # Wait for the input thread to finish
+    inp_thread.join()
     stdscr.nodelay(False)
-    key = stdscr.getch()
-    if key == ord('1'):
-
-      # Start the game
-
-        stdscr.nodelay(True)
-        game()
-    elif key == ord('2'):
-
-      # Display the configuration menu
-
-        config()
-    elif key == ord('3'):
-
-      # Quit the game
-
-        exit = 0
-
-
-
-
-# Main game loop
-
-while exit != 0:
-
-  # Clear the screen
-
     stdscr.clear()
-    menu()
-# Clean up and exit
+    stdscr.addstr('Game Over!', curses.color_pair(1))
+    stdscr.getch()
+        
+def spawn_food(board, snake, board_width, board_height):
+    while True:
+        y = random.randint(0, board_height - 1)
+        x = random.randint(0, board_width - 1)
+        if (y, x) not in snake:
+            return y, x
+            
+def change_board_size(stdscr):
+    stdscr.clear()
+    stdscr.addstr('Enter the new board height: ')
+    stdscr.refresh()
+    curses.echo()
+    board_height = stdscr.getstr()
+    stdscr.clear()
+    stdscr.addstr('Enter the new board width: ')
+    stdscr.refresh()
+    curses.echo()
+    board_width = stdscr.getstr()
+    board_height = clamp(int(board_height),10,50)
+    board_width = clamp(int(board_width),10,50)
+    return int(board_height), int(board_width)
 
-curses.nocbreak()
-stdscr.keypad(False)
-curses.echo()
-curses.endwin()
-print ('Game over!\n')
+def change_difficulty(stdscr):
+    stdscr.clear()
+    stdscr.addstr('Enter the new snake speed (in seconds): ')
+    stdscr.refresh()
+    curses.echo()
+    snake_speed = stdscr.getstr()
+    return float(snake_speed)
+
+def display_menu(stdscr):
+    if curses.has_colors():
+        curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        
+        
+    stdscr.clear()
+    stdscr.addstr('Welcome to Snake!\n\n', curses.color_pair(1))
+    stdscr.addstr('1. Start game\n', curses.color_pair(2))
+    stdscr.addstr('2. Change board size\n', curses.color_pair(2))
+    stdscr.addstr('3. Change difficulty\n', curses.color_pair(2))
+    stdscr.addstr('4. Quit\n\n', curses.color_pair(2))
+    # Highlight the selected menu option
+    stdscr.addstr(menu_selection, 1, ">", curses.A_REVERSE)
+    stdscr.refresh()
+
+    
+curses.wrapper(main)
